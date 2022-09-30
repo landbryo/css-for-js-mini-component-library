1f46f3d66f749bd69c4729125440ff81abcbdff8
Landon Otis <landon@7bidaho.com>
2022-09-30 11:35:36 -0700

    Add ProgressBar, IconInput, and Select components w/ styles.

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -1,19 +1,90 @@
 import React from 'react';
 import styled from 'styled-components';
+import Icon from "../Icon";
+import VisuallyHidden from "../VisuallyHidden";
+import {COLORS} from "../../constants";
 
-import { COLORS } from '../../constants';
-
-import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
+const STYLES = {
+    small: {
+        fontSize: 14,
+        strokeWidth: 1,
+        iconSize: 16,
+        height: 24,
+        borderThickness: 1
+    },
+    large: {
+        fontSize: 18,
+        strokeWidth: 2,
+        iconSize: 24,
+        height: 36,
+        borderThickness: 2
+    }
+};
 
 const IconInput = ({
-  label,
-  icon,
-  width = 250,
-  size,
-  placeholder,
-}) => {
-  return 'TODO';
+                       label,
+                       icon,
+                       width = 250,
+                       size,
+                       ...delegated
+                   }) => {
+
+    const styles = STYLES[size];
+
+    return (
+
+        <Wrapper>
+            <InputIcon id={icon} size={styles.iconSize} strokeWidth={styles.strokeWidth}/>
+            <VisuallyHidden>{label}</VisuallyHidden>
+            <Input style={{
+                '--height': styles.height + 'px',
+                '--width': width + 'px',
+                '--border-thickness': styles.borderThickness + 'px',
+                '--font-size': (styles.fontSize / 16) + 'rem'
+            }} {...delegated}/>
+        </Wrapper>
+    );
 };
+
+const Wrapper = styled.label`
+  position: relative;
+  color: ${COLORS.gray700};
+  display: block;
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const InputIcon = styled(Icon)`
+  color: inherit;
+  margin: auto 0;
+  pointer-events: none;
+  position: absolute;
+  bottom: 0;
+  top: 0;
+`;
+
+const Input = styled.input`
+  background: none;
+  border: none;
+  border-bottom: var(--border-thickness) solid ${COLORS.black};
+  color: inherit;
+  font-size: var(--font-size);
+  font-weight: 700;
+  height: var(--height);
+  padding-left: var(--height);
+  position: relative;
+  width: var(--width);
+
+  &::placeholder {
+    color: ${COLORS.gray500};
+    font-weight: 400;
+  }
+
+  &:focus {
+    outline-offset: 4px;
+  }
+`;
 
 export default IconInput;
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,64 @@
 import React from 'react';
 import styled from 'styled-components';
+import {getDisplayedValue} from './Select.helpers';
+import VisuallyHidden from "../VisuallyHidden";
+import Icon from "../Icon";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import {COLORS} from "../../constants";
 
-const Select = ({ label, value, onChange, children }) => {
-  const displayedValue = getDisplayedValue(value, children);
+const Select = ({label, value, onChange, children}) => {
+    const displayedValue = getDisplayedValue(value, children);
+    const width = (8 * displayedValue.length) + 64 + 'px';
 
-  return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
-  );
+    return (
+        <Wrapper>
+            <VisuallyHidden>
+                <label>{label}</label>
+            </VisuallyHidden>
+            <SelectInput width={width} value={value} onChange={onChange}>
+                {children}
+            </SelectInput>
+            <Arrow id="chevron-down" size="24" strokeWidth="2" />
+        </Wrapper>
+    );
 };
+
+const Wrapper = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  border-radius: 8px;
+  color: ${COLORS.gray700};
+  position: relative;
+  width: max-content;
+`;
+
+const SelectInput = styled.select`
+  background: none;
+  border: none;
+  border-radius: 8px;
+  color: inherit;
+  font-size: 1rem;
+  padding: 12px 16px;
+  position: relative;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+
+  width: ${p => p.width};
+  
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const Arrow = styled(Icon)`
+  color: inherit;
+  pointer-events: none;
+  position: absolute;
+  right: 16px;
+  top: 14px;
+  
+  ${SelectInput}:hover + & {
+    color: ${COLORS.black};
+  }
+`;
 
 export default Select;
